656af102b56a762c018f20815f51d2899d97b727
hyj5076 <hyj5076@hanmail.net>
2023-11-04 00:05:10 +0900

    test: 로또 번호 생성 테스트 성공

--- src/UI/RunLotteryMachine.js
+++ src/UI/RunLotteryMachine.js
@@ -2,5 +2,8 @@ import { MissionUtils } from "@woowacourse/mission-utils";
 
 export const runLotteryMachine = () => {
   const lottoNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+  console.log(lottoNumbers);
   return lottoNumbers;
 };
+
+runLotteryMachine();
