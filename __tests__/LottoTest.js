c3c18698af3e8c3fc5a688a797068a308a8716a8
hyj5076 <hyj5076@hanmail.net>
2023-11-04 16:32:11 +0900

    test: 로또 클래스 예외 발생 테스트 - 숫자 범위

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -13,13 +13,23 @@ describe("로또 클래스 테스트", () => {
     }).toThrow("[ERROR]");
   });
 
-  test("로또 번호에 숫자가 아닌 입력값이 있으면 예외가 발생한다.", () => {
+  test("로또 번호에 숫자가 아닌 값이 있으면 예외가 발생한다.", () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, "육"]);
     }).toThrow("[ERROR]");
 
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, ","]);
+    }).toThrow("[ERROR]");
+  });
+
+  test("로또 번호가 1에서 45 사이가 아니면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([0, 1, 2, 3, 4, 5]);
+    }).toThrow("[ERROR]");
+
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
     }).toThrow("[ERROR]");
   });
 });
