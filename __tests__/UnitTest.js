b88844ff84d69b2f67df0698425d6a2617fd441c
hyj5076 <hyj5076@hanmail.net>
2023-11-04 14:23:34 +0900

    test: 보너스 번호 출력 단위 테스트

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -1,9 +1,16 @@
 // UserLottoNumber.js 관련 단위 테스트
 describe("사용자 로또 번호", () => {
-  test("공백 무시하고 10진수의 배열로 받음", () => {
+  test("기본 로또 번호 - 공백 무시하고 10진수 배열로 받음", () => {
     const input = "5, 12,23 , 32,45, 52";
     const output = [5, 12, 23, 32, 45, 52];
     const result = input.split(",").map((number) => parseInt(number.trim(), 10));
+    expect(result).toEqual(output);
+  });
+
+  test("보너스 번호 - 공백 무시하고 10진수 숫자로 받음", () => {
+    const input = " 11 ";
+    const output = 11;
+    const result = parseInt(input.trim(), 10);
     expect(result).toEqual(output);
   });
 });
