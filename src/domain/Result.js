d05713b2011a718335c47bfbf5efded6e208c9c1
hyj5076 <hyj5076@hanmail.net>
2023-11-05 19:33:20 +0900

    feat: 수익률 소수점 둘째 자리까지 반올림 기능 구현

--- src/domain/Result.js
+++ src/domain/Result.js
@@ -1,8 +1,18 @@
 class Result {
-  constructor() {}
+  constructor(prize, payment) {
+    this.prize = prize;
+    this.payment = payment;
+  }
 
   returnRate() {
-    const rate = (prize / payment) * 100;
+    const rate = (this.prize / this.payment) * 100;
     return rate;
   }
+
+  rounding() {
+    const rate = this.returnRate();
+    return parseFloat(rate.toFixed(2));
+  }
 }
+
+export default Result;
