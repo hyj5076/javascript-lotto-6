4a9eed077e80690e01bf1a32f53909dc8ec73551
hyj5076 <hyj5076@hanmail.net>
2023-11-07 00:01:48 +0900

    feat(App): 로또 추첨 실행 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import Lotto from "./Lotto.js";
+import { runLotteryMachine } from "./domain/RunLotteryMachine.js";
+import UserBonusNumber from "./domain/UserBonusNumber.js";
+import UserPayment from "./domain/UserPayment.js";
+import { ask } from "./UI/inputView.js";
+
 class App {
-  async play() {}
+  constructor() {
+    this.lotto = new Lotto();
+    this.bonus = new UserBonusNumber();
+    this.payment = new UserPayment();
+  }
+
+  async play() {
+    try {
+      runLotteryMachine();
+      await this.setLotto();
+    } catch {}
+  }
+
+  async setLotto() {
+    const baseNums = await ask.baseNumbers();
+    const bonusNum = await ask.bonusNumber();
+    const pay = await ask.payment();
+
+    this.lotto.setUserLottoNumbers(baseNums);
+    this.bonus.setBonusNumber(bonusNum);
+    this.payment.setUserPayment(pay);
+  }
+
+  runLotteryMachine() {
+    const machineNumbers = runLotteryMachine();
+  }
 }
 
 export default App;
