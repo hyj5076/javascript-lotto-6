e5f2e7f84aea0d071b0382fb8e94af136e8cbab2
hyj5076 <hyj5076@hanmail.net>
2023-11-06 22:23:10 +0900

    refactor: 보너스 숫자 클래스 private 필드 선언 및 동기 함수로 변경

--- src/domain/UserBonusNumber.js
+++ src/domain/UserBonusNumber.js
@@ -1,32 +1,31 @@
-import { readUserInput } from "../UI/inputView.js";
 import { ERROR } from "../const/Messages.js";
 
 class UserBonusNumber {
+  #bonusNumber;
+
   constructor() {
-    this.bonusNumber = null;
+    this.#bonusNumber = null;
   }
 
-  async getBonusNumber() {
-    const input = await readUserInput();
-    this.setBonusNumbers(input);
-    return this.bonusNumber;
+  getBonusNumber(input) {
+    this.setBonusNumber(input);
+    return this.#bonusNumber;
+  }
+
+  setBonusNumber(input) {
+    const trimmedInput = input.trim();
+    const parsedNumber = parseInt(trimmedInput, 10);
+    this.#validate(parsedNumber);
+    this.#bonusNumber = parsedNumber;
   }
 
   #validate(number) {
     if (isNaN(number)) {
       throw new Error(ERROR.ONE_NUMBER);
     }
-
     if (number < 1 || number > 45) {
       throw new Error(ERROR.NUMBER_RANGE);
     }
-  }
-
-  setBonusNumbers(input) {
-    const trimmedInput = input.trim();
-    const parsedNumber = parseInt(trimmedInput, 10);
-    this.#validate(parsedNumber);
-    this.bonusNumber = parsedNumber;
   }
 }
 
