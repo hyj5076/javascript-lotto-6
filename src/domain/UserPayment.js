e2395f2b55ec285bef8b86198995cd384e1a1540
hyj5076 <hyj5076@hanmail.net>
2023-11-07 23:43:10 +0900

    refactor: 티켓수 출력

--- src/domain/UserPayment.js
+++ src/domain/UserPayment.js
@@ -1,4 +1,4 @@
-import { ERROR } from "../const/Messages.js";
+import { ERROR, PRINT } from "../const/Messages.js";
 
 class UserPayment {
   constructor() {
@@ -26,7 +26,8 @@ class UserPayment {
       return 0;
     }
     const ticketCounts = this.payment / 1000;
-    return ticketCounts;
+    const printTicketCounts = `${ticketCounts}${PRINT.HOW_MANY_BUY}`;
+    return printTicketCounts;
   }
 }
 
