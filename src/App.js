ad8be3fbd43722b62047b46d45924758d5d4efd4
hyj5076 <hyj5076@hanmail.net>
2023-11-07 19:31:52 +0900

    test: 가장 정답에 근접한 테스트

--- src/App.js
+++ src/App.js
@@ -8,21 +8,45 @@ import UserBaseNumbers from "./domain/UserBaseNumbers.js";
 import LottoEvaluator from "./domain/LottoEvaluator.js";
 
 class App {
-  constructor() {}
+  constructor() {
+    this.base = new UserBaseNumbers();
+    this.bonus = new UserBonusNumber();
+    this.payment = new UserPayment();
+    this.lottoEvaluator = null;
+  }
 
   async play() {
     try {
       this.makeLotto();
+      await this.setLotto();
     } catch (error) {
       Console.print(error.message);
     }
   }
 
-  async setLotto() {}
+  async setLotto() {
+    // 사용자 입력값 호출
+    const pay = await ask.payment();
+    const baseNums = await ask.baseNumbers();
+    const bonusNum = await ask.bonusNumber();
+
+    // 로또 유효성 검사와 함께 저장
+    this.payment.setUserPayment(pay);
+    this.base.setBaseNumbers(baseNums);
+    this.bonus.setBonusNumber(bonusNum);
+
+    // 티켓과 기계 번호를 평가
+    const numberOfTickets = this.payment.numberOfTickets();
+
+    Console.print(`지불금액: ${pay}`);
+    Console.print(`티켓수: ${numberOfTickets}`);
+    Console.print(`기본숫자: ${baseNums}`);
+    Console.print(`보너스숫자: ${bonusNum}`);
+  }
 
   makeLotto() {
     const lottoNumbers = runLotteryMachine();
-    Console.print(`Generated Lotto Numbers: ${lottoNumbers}`);
+    Console.print(`기계로또: ${lottoNumbers}`);
   }
 }
 
