4f0a8057c9f6fdb49711f77cecc50406c6e18fbb
hyj5076 <hyj5076@hanmail.net>
2023-11-07 19:02:06 +0900

    test: 로또 랜덤수 생성

--- src/App.js
+++ src/App.js
@@ -5,48 +5,24 @@ import UserPayment from "./domain/UserPayment.js";
 import { ask } from "./UI/inputView.js";
 import { Console } from "@woowacourse/mission-utils";
 import UserBaseNumbers from "./domain/UserBaseNumbers.js";
+import LottoEvaluator from "./domain/LottoEvaluator.js";
 
 class App {
-  constructor() {
-    this.base = new UserBaseNumbers();
-    this.bonus = new UserBonusNumber();
-    this.payment = new UserPayment();
-  }
+  constructor() {}
 
   async play() {
     try {
-      runLotteryMachine();
-      await this.setLotto();
+      this.makeLotto();
     } catch (error) {
       Console.print(error.message);
     }
   }
 
-  async setLotto() {
-    // 사용자 입력값 호출
-    const pay = await ask.payment();
-    const baseNums = await ask.baseNumbers();
-    const bonusNum = await ask.bonusNumber();
+  async setLotto() {}
 
-    // 로또 유효성 검사
-    this.payment.setUserPayment(pay);
-    this.base.setBaseNumbers(baseNums);
-    this.bonus.setBonusNumber(bonusNum);
-
-    //const lotto = new Lotto(baseNums);
-    //lotto.ascendingOrder();
-
-    Console.print(pay);
-    Console.print(baseNums);
-    Console.print(bonusNum);
-  }
-
-  evaluator() {
-    LottoEvaluator();
-  }
-
-  runLotteryMachine() {
-    //const machineNumbers = runLotteryMachine();
+  makeLotto() {
+    const lottoNumbers = runLotteryMachine();
+    Console.print(`Generated Lotto Numbers: ${lottoNumbers}`);
   }
 }
 
